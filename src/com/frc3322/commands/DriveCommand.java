cfd80f812392bb46a735d274812ddb6bc9e860b7
Sayan Raychaudhuri <raychaudhurisayan@gmail.com>
2018-11-17 14:07:55 -0500

    Added drive command

---
+++ src/com/frc3322/commands/DriveCommand.java
@@ -0,0 +1,28 @@
+package com.frc3322.commands;
+
+import com.frc3322.RobotMap;
+import edu.wpi.first.wpilibj.command.Command;
+
+import static com.frc3322.Robot.drivetrain;
+
+public class DriveCommand extends Command{
+
+    private final int HORIZONTAL_AXIS;
+    private final int VERTICAL_AXIS;
+    private final int ROTATION_AXIS;
+
+    public DriveCommand(){
+
+        requires(drivetrain);
+
+        HORIZONTAL_AXIS = RobotMap.XBOX.STICK_L_X_AXIS;
+        VERTICAL_AXIS = RobotMap.XBOX.STICK_L_Y_AXIS;
+        ROTATION_AXIS = RobotMap.XBOX.STICK_R_X_AXIS;
+
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+}
